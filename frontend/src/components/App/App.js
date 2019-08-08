55cf3e129d386002253a745b3d3188c2df97963c
Chris <cjmarsh725@gmail.com>
2019-08-08 11:56:59 -0700

    Retrieve skills via axios call

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -19,18 +19,42 @@ class App extends Component {
   componentDidMount() {
     axios.get("https://api.guildwars2.com/v2/professions").then(response => {
       const professionList = response.data;
-      const requests = [];
+      const profRequests = [];
       professionList.forEach(profession => {
-        requests.push(axios.get(`https://api.guildwars2.com/v2/professions/${profession}`));
+        profRequests.push(axios.get(`https://api.guildwars2.com/v2/professions/${profession}`));
       });
       const professions = {};
-      axios.all(requests).then(response => {
+      axios.all(profRequests).then(response => {
+        const profData = response.map(res => res.data);
         professionList.forEach(profession => {
-          professions[profession] = response.map(res => res.data).find(data => data.id === profession);
+          professions[profession] = profData.find(data => data.id === profession);
         });
+        axios.get("https://api.guildwars2.com/v2/skills?ids=all").then(response => {
+          console.log("All: " + response.data.length + " | Size: " + JSON.stringify(response.data).length);
+        }).catch(error => console.error("All skills request: " + error));
+        const skillRequests = [];
+        profData.forEach(data => {
+          Object.keys(data.weapons).forEach(weapon => {
+            data.weapons[weapon].skills.forEach(skill => {
+              skillRequests.push(`https://api.guildwars2.com/v2/skills/${skill.id}`);
+            });
+          });
+          data.training.forEach(training => {
+            if (training.category === "Skills" || training.category === "EliteSpecializations") {
+              training.track.forEach(track => {
+                if (track.skill_id) {
+                  skillRequests.push(`https://api.guildwars2.com/v2/skills/${track.skill_id}`);
+                }
+              })
+            }
+          });
+        });
+        axios.all(skillRequests).then(response => {
+          console.log("Profession skills: " + response.length);
+        }).catch(error => console.error("Profession skills request: " + error));
         this.setState({ professionList, professions });
-      }).catch(error => console.error("Individual professions endpoint: " + error));
-    }).catch(error => console.error("Profession list endpoint: " + error));
+      }).catch(error => console.error("Individual professions request: " + error));
+    }).catch(error => console.error("Profession list request: " + error));
   }
 
   render() {
