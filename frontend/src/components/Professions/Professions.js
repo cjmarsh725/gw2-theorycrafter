34a93545fd37c341202f45d6c12cb669a51e5c45
Chris <cjmarsh725@gmail.com>
2019-08-11 15:23:18 -0700

    Made profession buttons smaller

--- frontend/src/components/Professions/Professions.js
+++ frontend/src/components/Professions/Professions.js
@@ -13,7 +13,7 @@ class Professions extends Component {
                 className={"professions-select-btn" + (currentProfession === prof ? 
                                                 " professions-select-btn-active" : "")}
                 onClick={() => changeProfession(prof)}>
-              <img alt={prof + " icon"} src={professions[prof].icon_big} />
+              <img alt={prof + " icon"} src={professions[prof].icon} />
               <div className="professions-select-btn-label">{prof}</div>
             </div>
           );
